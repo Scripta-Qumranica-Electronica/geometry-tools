135c1179cf3af2026f222f7e2d6416bfa156b674
Bronson Brown-deVost <bronsonbdevost@aim.com>
2020-03-27 11:23:14 +0100

    added error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! # The Geometry Tools Library
+//!
+//! The geometry tools library provides functionality to interoperate between
+//! SVG and WellKnown geometry types. It offers a set of validation functions
+//! along with the standard geometric boolean operations.
+//!
 mod geometry_boolean;
 mod geometry_wkt_writer;
 mod utils;
@@ -18,127 +24,79 @@ extern "C" {
     fn alert(s: &str);
 }
 
+/// Shows a greeting in an alert (remove for production).
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, geometry-tools!");
 }
 
-#[wasm_bindgen]
-pub fn wktUnion(geom1: String, geom2: String) -> String {
-    //return "".into();
+/// Generates a union from two WKT geometries.
+/// It throws an error if the union of the two geometry types is not supported,
+/// or if invalid geometries have been submitted.
+///
+#[wasm_bindgen(js_name = wktUnion)]
+pub fn wkt_union(geom1: String, geom2: String) -> Result<String, JsValue> {
+    let union_err = "The submitted geometries cannot be unioned.";
     // Grab the stated type of each input
-    let mut geom1_type = String::from("");
-    match geom1.get(..1) {
-        Some("G") => geom1_type = String::from("GeometryCollection"),
-        Some("L") => geom1_type = String::from("LineString"),
-        Some("M") => {
-            if geom1.starts_with("MultiL") {
-                geom1_type = String::from("MultiLineString");
-            } else if geom1.starts_with("MultiPoi") {
-                geom1_type = String::from("MultiPoint");
-            } else if geom1.starts_with("MultiP") {
-                geom1_type = String::from("MultiPolygon");
-            }
-        }
-        Some("P") => {
-            if geom1.starts_with("Poi") {
-                geom1_type = String::from("Point");
-            } else {
-                geom1_type = String::from("Polygon");
-            }
-        }
-        // Return immediately on empty string
-        Some(&_) => return "".into(),
-        None => return "".into(),
-    }
-    let mut geom2_type = String::from("");
-    match geom2.get(..1) {
-        Some("G") => geom2_type = String::from("GeometryCollection"),
-        Some("L") => geom2_type = String::from("LineString"),
-        Some("M") => {
-            if geom2.starts_with("MultiL") {
-                geom2_type = String::from("MultiLineString");
-            } else if geom2.starts_with("MultiPoi") {
-                geom2_type = String::from("MultiPoint");
-            } else if geom2.starts_with("MultiP") {
-                geom2_type = String::from("MultiPolygon");
-            }
-        }
-        Some("P") => {
-            if geom2.starts_with("Poi") {
-                geom2_type = String::from("Point");
-            } else {
-                geom2_type = String::from("Polygon");
-            }
-        }
-        // Return immediately on empty string
-        Some(&_) => return "".into(),
-        None => return "".into(),
-    }
+    let geom1_type = get_geometry_type(&geom1)?;
+    let geom2_type = get_geometry_type(&geom2)?;
 
-    if geom1_type == "MultiPolygon" {
-        if geom1_type == geom2_type {
+    if geom1_type.eq_ignore_ascii_case("MultiPolygon")  {
+        return if geom1_type == geom2_type {
             //union two multipolygons
-            return wkt_multi_polygon_union(geom1, geom2);
-        } else if geom2_type == "Polygon" {
+            Ok(wkt_multi_polygon_union(geom1, geom2))
+        } else if geom2_type.eq_ignore_ascii_case("Polygon") {
             // union multi + polygon
-            return wkt_multi_polygon_polygon_union(geom1, geom2);
+            Ok(wkt_multi_polygon_polygon_union(geom1, geom2))
         } else {
-            return "".into();
-        } // Return immediately
-    }
-
-    if geom1_type == "Polygon" {
-        if geom1_type == geom2_type {
+            Err(JsValue::from_str(union_err))
+        }
+    } else if geom1_type.eq_ignore_ascii_case("Polygon") {
+        return if geom1_type == geom2_type {
             // union two polygons
-            return wkt_polygon_union(geom1, geom2);
-        } else if geom2_type == "MulitPolygon" {
+            Ok(wkt_polygon_union(geom1, geom2))
+        } else if geom2_type.eq_ignore_ascii_case("MulitPolygon") {
             // union multi + polygon
-            return wkt_multi_polygon_polygon_union(geom2, geom1);
+            Ok(wkt_multi_polygon_polygon_union(geom2, geom1))
         } else {
-            return "".into();
-        } // Return immediately
+            Err(JsValue::from_str(union_err))
+        }
     }
 
-    return "".into();
+    Err(JsValue::from_str("Could not process the submitted geometries."))
 }
 
-// #[wasm_bindgen]
-// pub fn shape(input_shape: String) -> String {
-//     let new_shape: Wkt<f64> = Wkt::from_str(
-//         &input_shape
-//     )
-//     .ok()
-//     .unwrap();
-
-//     let geom = wkt::conversion::try_into_geometry(&new_shape.items[0])
-//         .ok()
-//         .unwrap();
-
-//     let geom_poly = geom.into_polygon().unwrap();
-
-//     let poly = polygon![
-//         (x: 1.0, y: 1.0),
-//         (x: 4.0, y: 1.0),
-//         (x: 4.0, y: 4.0),
-//         (x: 1.0, y: 4.0),
-//         (x: 1.0, y: 1.0),
-//     ];
-
-//     // // Calculate the polygon's convex hull
-//     let hull = poly.convex_hull();
-
-//     let union: geo::MultiPolygon<f64> = geom_poly.xor(&poly);
-//     let simp_union = union.simplify(&1.0);
-//     let simp_poly = &simp_union.0.iter().next().unwrap();
-
-//     format!("{} {} {} {}", geom_poly.to_wkt(), poly.to_wkt(),
-//     union.to_wkt(), simp_poly.to_wkt())
-// }
-
-#[wasm_bindgen]
-pub fn shoutOut() -> String {
-    "Hey I returned a string!".into()
+/// This function reads a submitted string and makes a very quick decision
+/// about the WKT geometry type it must contain. This function does not
+/// actually check if the string really contains the shape it claims to
+/// contain, nor does it do any processing or validation. All validation should
+/// be down downstream.
+///
+fn get_geometry_type(geom: &String) -> Result<String, JsValue> {
+    let geom_parse_error = "One or both of the submitted geometries is invalid/unsupported.";
+    match geom.get(..1) {
+        Some("G") => Ok(String::from("GeometryCollection")),
+        Some("L") => Ok(String::from("LineString")),
+        Some("M") => {
+            if geom.starts_with("MultiL") {
+                Ok(String::from("MultiLineString"))
+            } else if geom.starts_with("MultiPoi") {
+                Ok(String::from("MultiPoint"))
+            } else if geom.starts_with("MultiP") {
+                Ok(String::from("MultiPolygon"))
+            } else { Err(JsValue::from_str("Input error.")) }
+        }
+        Some("P") => {
+            if geom.starts_with("Poi") {
+                Ok(String::from("Point"))
+            } else {
+                Ok(String::from("Polygon"))
+            }
+        }
+        // Return immediately on empty string
+        Some(&_) => Err(JsValue::from_str(geom_parse_error)),
+        None => Err(JsValue::from_str(geom_parse_error)),
+    }
 }
 
 #[cfg(test)]
@@ -147,14 +105,14 @@ mod tests {
 
     #[test]
     fn can_greet() {
-        let msg = shoutOut();
+        let msg = shout_out();
         let expected = String::from("Hey I returned a string!");
         assert_eq!(expected, msg);
     }
 
     #[test]
     fn can_join_polygons() {
-        let res = wktUnion(
+        let res = wkt_union(
             "POLYGON((0 0,10 0,10 10,0 10,0 0),(3 3,6 3,6 6,3 6,3 3))".into(),
             "POLYGON((2 2,4 2,4 4,2 4,2 2))".into(),
         );
